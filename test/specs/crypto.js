359f74977c2d461bda4f4ab618d01d252dd709d9
Jonn <jm@memorici.de>
2021-08-10 18:49:52 +0100

    Problem: No way to test in-browser JS w/o browser
    Solution:
    
     - Use WebdriverIO
     - Configure headless tests (no XORG, works in WSL)
     - Make sure that semple tests are running

---
+++ test/specs/crypto.js
@@ -0,0 +1,5 @@
+describe('doauthor cryptography suite', () => {
+    it('decodes and encodes base64 in a way that is compatible with the reference implementation', () => {
+        true;
+    })
+})
