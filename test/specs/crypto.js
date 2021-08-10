20274a680786135e4401e53a3d218432f87f3d59
Jonn <jm@memorici.de>
2021-08-10 21:29:15 +0100

    Problem: No way to test local code
    Solution:
    
     1. Add test:setup that serves local directory
     2. Add HTML that we're actually going to test
     3. Add test that tests browser's title
    
    Run:
    
     1. Run in one terminal "npm run test:setup"
     2. Run in another terminal "npm run test:run"
    
    CI:
    
     Since CI is set up and tore down, you can simply run "npm run test"
    which will background npx serve, but not clean up. We probably should
    figure out how to clean up, but eh.

--- test/specs/crypto.js
+++ test/specs/crypto.js
@@ -1,5 +1,9 @@
+const assert = require('assert/strict');
+
 describe('doauthor cryptography suite', () => {
-    it('decodes and encodes base64 in a way that is compatible with the reference implementation', () => {
-        true;
+    it('decodes and encodes base64 in a way that is compatible with the reference implementation', async () => {
+        await browser.url('http://localhost:8174/example.html');
+        const title = await browser.getTitle();
+        assert.equal(title, "doauthor demo");
     })
 })
